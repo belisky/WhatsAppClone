942c0587c1151f5d47de4abfb54624630ac0fd61
bel <denoblesnobility2@gmail.com>
2022-01-04 16:37:08 +0000

    added uuid library to provide unique ids

--- clientside/src/components/Login.jsx
+++ clientside/src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React,{useRef} from 'react'
-import { Container,Form,Button } from 'react-bootstrap'
+import { Container, Form, Button } from 'react-bootstrap'
+import {v4 as uuidv4} from 'uuid'
 
 const Login = ({onIdSubmit}) => {
     const idRef = useRef('')
@@ -8,6 +9,9 @@ const Login = ({onIdSubmit}) => {
         onIdSubmit(idRef.current.value)
         
     }
+    const createNewId = () => {
+        onIdSubmit(uuidv4())
+    }
     return (
         <Container className='align-items-center d-flex' style={{height:'100vh'}}>
             <Form onSubmit={handleSubmit} className='w-100'>
@@ -15,8 +19,8 @@ const Login = ({onIdSubmit}) => {
                     <Form.Label>Enter Your Id</Form.Label>
                     <Form.Control type='text' ref={idRef} required/>
                 </Form.Group>
-                <Button type='submit'>Login</Button>
-                <Button  variant='secondary'>Create a New id</Button>
+                <Button type='submit' className='mr-2'>Login</Button>
+                <Button onClick={createNewId} variant='secondary'>Create a New id</Button>
             </Form>
         </Container>
     )
