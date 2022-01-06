c4c7cae8f828a108ca935bb9b92ebfc4d8dd5005
bel <denoblesnobility2@gmail.com>
2022-01-06 15:20:16 +0000

    added a usecallback hook to keep current texts into view

--- clientside/src/components/OpenConversation.jsx
+++ clientside/src/components/OpenConversation.jsx
@@ -1,9 +1,16 @@
-import React ,{useState} from 'react'
+import React ,{useState,useCallback } from 'react'
 import {Form, InputGroup,Button} from 'react-bootstrap'
 import { useConversations } from '../Helpers/Contexts/ConversationsProvider'
 const OpenConversation = () => {
     const {sendMessage,selectedConversation}=useConversations()
     const [text, setText] = useState('')
+  
+    const setRef = useCallback(node => {
+        if (node) {
+            
+            node.scrollIntoView({smooth:true})
+        }
+    },[])
     const handleSubmit = (e) => {
         e.preventDefault()
         sendMessage(selectedConversation.recipients.map(r => r.id),
@@ -11,13 +18,17 @@ const OpenConversation = () => {
         )
         setText('')
     }
+   
     return (
         <div className='d-flex flex-column flex-grow-1'>
             <div className="flex-grow-1 overflow-auto">
-                <div className="h-100 d-flex flex-column align-items-start justify-content-end px-3">
+                <div className="d-flex flex-column align-items-start justify-content-end px-3">
                     {selectedConversation.messages.map((message, index) => {
+                        const lastMessage=selectedConversation.messages.length-1===index
                         return (
-                         <div key={index} className="my-1 d-flex flex-column">
+                            <div
+                                ref={lastMessage?setRef:null}
+                                key={index} className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end' : ''}`}>
                                 <div className={`rounded px-2 py-1 ${message.fromMe ? 'bg-primary text-white' : 'border}'}`}>
                                     {message.text}
                                 </div>
